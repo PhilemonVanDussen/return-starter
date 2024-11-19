40327845f5e44e51f60c6cb2fd2239d262dbb1e9
PhilemonVanDussen <vandussenph709@northwested.org>
2024-11-19 14:29:47 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# PJ VanDussen
+# 11/192024
+# Return Starter
+
+# Project 1
+def describe_vacation(destination, activity, season = 'summer'):
+    #  description1 = print(f'I am going to {destination} and do stuff like {activity} in the {season}')
+    #  return description1
+    return f'I am going to {destination} and do stuff like {activity} in the {season}.'
+
+description1 = describe_vacation('Japan', 'eating shushi')
+description2 = describe_vacation('Iceland', 'visit volcanoes')
+print(description1)
+print(description2)
+
+
+# def describe_vacation(destination, activity, season):   
+#     description2 = print(f'Have you ever been to {destination} and {activity}? It\'s great in the {season}')
+#     return description2
+# describe_vacation('London', 'go to a theater', 'spring')
+
+# # Project 2
+# def show_major(first_name, university, major):
+     
